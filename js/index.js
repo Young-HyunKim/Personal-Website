8b0d7173c1c6b40163eab02b23a47c46b20f66e4
Young-Hyun Kim <contact@younghyun.kr>
2019-02-03 09:54:38 +0900

    Change : index.js Logic

--- js/index.js
+++ js/index.js
@@ -9,14 +9,18 @@ $(document).ready(function () {
 
     function SetBackgroundColor(){
         
-        if (0<= Today <=6) {
-            ColorCode = "#1976d2";
-        } else if(7<= Today <=12) {
-            ColorCode = "#64b5f6";
-        } else if(13<= Today <=18) {
-            ColorCode = "#ffa000";
-        } else if(19<= Today <=23){
-            ColorCode = "#1a237e";
+        if ( 0 <= ColorCode && ColorCode <= 6 ) {
+            console.log(1);
+            $('#ScreenColor').css('background','#1976d2');
+        } else if( 7 <= ColorCode && ColorCode <= 12 ) {
+            console.log(2);
+            $('#ScreenColor').css('background','#64b5f6');
+        } else if( 13 <= ColorCode && ColorCode <= 18 ) {
+            console.log(3);
+            $('#ScreenColor').css('background','#ffa000');
+        } else if( 19 <= ColorCode && ColorCode <= 23 ){
+            console.log(4);
+            $('#ScreenColor').css('background','#1a237e');
         }
 
     }
