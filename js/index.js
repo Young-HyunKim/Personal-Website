ffc0f06dad9c1ef818ee8f36b144c4fe8050dad7
Young-Hyun Kim <contact@younghyun.kr>
2019-02-08 03:12:48 +0900

    Change : Log Content

--- js/index.js
+++ js/index.js
@@ -24,36 +24,36 @@ $(document).ready(function () {
     function SetBackgroundColor(){
         
         if ( 0 <= ColorCode && ColorCode <= 2 ) {
-            console.log(1);
+            console.log("0시 ~ 2시");
             //$('#ScreenColor').css('background','#1a237e');
             $('#ScreenColor').css('background','linear-gradient(to left, #1a237e, #283593, #303f9f, #3949ab, #3f51b5)');
         } else if( 3 <= ColorCode && ColorCode <= 5 ) {
-            console.log(2);
+            console.log("3시 ~ 5시");
             //$('#ScreenColor').css('background','#283593');
             $('#ScreenColor').css('background','linear-gradient(to left, #0d47a1, #1565c0, #1976d2, #1e88e5, #2196f3)');
         } else if( 6 <= ColorCode && ColorCode <= 8 ) {
-            console.log(3);
+            console.log("6시 ~ 8시");
             //$('#ScreenColor').css('background','#303f9f');
             $('#ScreenColor').css('background','linear-gradient(to left, #03a9f4, #29b6f6, #4fc3f7, #81d4fa, #b3e5fc)');
         } else if( 9 <= ColorCode && ColorCode <= 11 ){
-            console.log(4);
+            console.log("9시 ~ 11시");
             //$('#ScreenColor').css('background','#2196f3');
             $('#ScreenColor').css('background','linear-gradient(to left, #01579b, #0277bd, #0288d1, #039be5, #03a9f4)');
         } else if( 12 <= ColorCode && ColorCode <= 14 ){
-            console.log(5);
+            console.log("12시 ~ 14시");
             //$('#ScreenColor').css('background','#8c9eff');
             $('#ScreenColor').css('background','linear-gradient(to left, #01579b, #0277bd, #0288d1, #039be5, #03a9f4)');
         } else if( 15 <= ColorCode && ColorCode <= 17 ){
-            console.log(6);
+            console.log("15시 ~ 17시");
             //$('#ScreenColor').css('background','#f06292');
             $('#ScreenColor').css('background','linear-gradient(to left, #ec407a, #f06292, #f48fb1, #f8bbd0, #fce4ec)');
         } else if( 18 <= ColorCode && ColorCode <= 20 ){
-            console.log(7);
+            console.log("18시 ~ 20시");
             //$('#ScreenColor').css('background','#e91e63');
             $('#ScreenColor').css('background','linear-gradient(to left, #880e4f, #ad1457, #c2185b, #d81b60, #e91e63)');
 
         } else if( 21 <= ColorCode && ColorCode <= 23  ){
-            console.log(8);
+            console.log("21시 ~ 23시");
             //$('#ScreenColor').css('background','#9c27b0');
             $('#ScreenColor').css('background','linear-gradient(to left, #4a148c, #6a1b9a, #7b1fa2, #8e24aa,#9c27b0)');
         }
