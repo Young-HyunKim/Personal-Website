347323133804ca901ee6db5ef34b4c0f57482072
Young-Hyun Kim <contact@younghyun.kr>
2019-02-22 02:14:47 +0900

    Change : Ajax / Jsonp

--- js/AjaxTest.js
+++ js/AjaxTest.js
@@ -1,36 +1,15 @@
 $(document).ready(function() {
-
-    jQuery.ajax({
-
-          type:"GET",
-
-          url:"http://younghyun.kr/php/JSON_Select.php",
-
-          dataType:"JSON", // 옵션이므로 JSON으로 받을게 아니면 안써도 됨
-
-          success : function(data) {
-
-            $.each(data, function() {
-
-                console.log(data.Date);
-            
-            });
-           
-
-          },
-
-          complete : function(data) {
-
-
-          },
-
-          error : function(xhr, status, error) {
-
-                alert("에러발생");
-
-          }
-
-    });
+    
+    $.ajax({
+        url: "http://younghyun.kr/php/JSON_Select.php",
+        dataType: 'jsonp',
+        success: function(data) {
+          console.log('성공 - ', data);
+        },
+        error: function(xhr) {
+          console.log('실패 - ', xhr);
+        }
+      });
 
 });
 
